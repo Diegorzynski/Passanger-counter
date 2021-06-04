94dc2b3f478c499763cb5a9d2fd3d0d83ae12677
Diego Gorzynski <diegorzynski@hotmail.com>
2021-06-04 20:08:14 +0200

    js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 
 //let count = 0
 
+
+
+// initialize as 0 
+// listen for clicks to increment
+// increment the CountQueuingStrategychange the count-el in the html
+
+function increment() {
+    console.log("the button was clicked")
+}
