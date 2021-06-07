1a2e72c803f0f54b2a734a842f1b98d196777261
Diegorzynski <diegorzynski@hotmail.com>
2021-06-07 10:22:47 +0200

    added save

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 let countEl = document.getElementById("count-el")
-
-
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
   count ++
   countEl.innerText = count
-  console.log(count)
-}
+  }
 let saveBtn = getElementById("save-btn")
 
+
+
 function save() {
+let countStr  = count + " - ";
+saveEl.innerText += countStr;
+  
     console.log(count)
     }
+  
+   
+
 
 
 
